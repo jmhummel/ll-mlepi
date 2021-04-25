c9e94e0ddd6793dddb0aeaa4d54939924b4f2e20
Jeremy Hummel <jeremy.hummel@rackspace.com>
2021-04-25 16:06:29 -0700

    Update train function to use CLI args

--- train.py
+++ train.py
@@ -1,13 +1,26 @@
 from tensorflow import keras
 from networks import ResNet
+import argparse
 
+DATASETS = {
+    'cifar10': (keras.datasets.cifar10, 3, 32, 10),
+    'cifar100': (keras.datasets.cifar100, 3, 32, 100),
+    'fashion_mnist': (keras.datasets.fashion_mnist, 1, 28, 10),
+    'mnist': (keras.datasets.mnist, 1, 28, 10),
+}
 
-def train():
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+def train(
+        batch_size  = 64,       # Batch size
+        epochs      = 25,       # Number of epochs
+        dataset     = 'mnist',  # Dataset to use
+        layer_depth = 50,       # Layer depth of ResNet
+):
+    data_source, num_channels, resolution, label_size = DATASETS['dataset']
+    (x_train, y_train), (x_test, y_test) = data_source.load_data(dataset)
     x_train = x_train.astype("float32") / 255
     x_test = x_test.astype("float32") / 255
 
-    model = ResNet(num_channels=3, resolution=32, label_size=10, layer_depth=50)
+    model = ResNet(num_channels=num_channels, resolution=resolution, label_size=label_size, layer_depth=layer_depth)
     print(model.summary())
 
     model.compile(
@@ -16,7 +29,7 @@ def train():
         metrics=["accuracy"],
     )
 
-    history = model.fit(x_train, y_train, batch_size=64, epochs=25, validation_split=0.2)
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
 
     test_scores = model.evaluate(x_test, y_test, verbose=2)
     print("Test loss:", test_scores[0])
@@ -27,4 +40,11 @@ def train():
 # Calls the function indicated in config.py.
 
 if __name__ == "__main__":
-    train()
+    parser = argparse.ArgumentParser(description='Train ResNet')
+    parser.add_argument('-b', '--batch-size', type=int, action='store', dest='batch_size', help='Batch size')
+    parser.add_argument('-e', '--epochs', type=int, action='store', help='Number of epochs to train over')
+    parser.add_argument('-d', '--dataset', choices=DATASETS.keys(), help='Dataset to use')
+    parser.add_argument('-l', '--layer-depth', type=int, action='store', dest='layer_depth', help='ResNet layer depth')
+    args = parser.parse_args()
+    print(f'Training: {args}')
+    train(**vars(args))

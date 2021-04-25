a8b2555cc229627aceb1da9cee33bf7b6854f30d
Jeremy Hummel <jeremy.hummel@rackspace.com>
2021-04-25 15:10:52 -0700

    init

---
+++ networks.py
@@ -0,0 +1,129 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+#---------------------------------------------------------------------------
+# Residual block used in ResNet-18, ResNet-34
+
+def block0(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
+    """A residual block.
+    Arguments:
+      x: input tensor.
+      filters: integer, filters of the bottleneck layer.
+      kernel_size: default 3, kernel size of the bottleneck layer.
+      stride: default 1, stride of the first layer.
+      conv_shortcut: default True, use convolution shortcut if True,
+          otherwise identity shortcut.
+      name: string, block label.
+    Returns:
+      Output tensor for the residual block.
+    """
+    bn_axis = 3 if keras.backend.image_data_format() == 'channels_last' else 1
+
+    if conv_shortcut:
+        shortcut = layers.Conv2D(filters, 1, strides=stride, name=name + '_0_conv')(x)
+        shortcut = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_0_bn')(shortcut)
+    else:
+        shortcut = x
+
+    x = layers.Conv2D(filters, kernel_size, strides=stride, name=name + '_1_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_1_bn')(x)
+    x = layers.Activation('relu', name=name + '_1_relu')(x)
+
+    x = layers.Conv2D(filters, kernel_size, padding='SAME', name=name + '_2_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_2_bn')(x)
+
+    x = layers.Add(name=name + '_add')([shortcut, x])
+    x = layers.Activation('relu', name=name + '_out')(x)
+    return x
+
+#---------------------------------------------------------------------------
+# Residual block used in ResNet-50, 101, 152
+
+def block1(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
+    """A residual block.
+    Arguments:
+      x: input tensor.
+      filters: integer, filters of the bottleneck layer.
+      kernel_size: default 3, kernel size of the bottleneck layer.
+      stride: default 1, stride of the first layer.
+      conv_shortcut: default True, use convolution shortcut if True,
+          otherwise identity shortcut.
+      name: string, block label.
+    Returns:
+      Output tensor for the residual block.
+    """
+    bn_axis = 3 if keras.backend.image_data_format() == 'channels_last' else 1
+
+    if conv_shortcut:
+        shortcut = layers.Conv2D(4 * filters, 1, strides=stride, name=name + '_0_conv')(x)
+        shortcut = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_0_bn')(shortcut)
+    else:
+        shortcut = x
+
+    x = layers.Conv2D(filters, 1, strides=stride, name=name + '_1_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_1_bn')(x)
+    x = layers.Activation('relu', name=name + '_1_relu')(x)
+
+    x = layers.Conv2D(filters, kernel_size, padding='SAME', name=name + '_2_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_2_bn')(x)
+    x = layers.Activation('relu', name=name + '_2_relu')(x)
+
+    x = layers.Conv2D(4 * filters, 1, name=name + '_3_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5, name=name + '_3_bn')(x)
+
+    x = layers.Add(name=name + '_add')([shortcut, x])
+    x = layers.Activation('relu', name=name + '_out')(x)
+    return x
+
+#---------------------------------------------------------------------------
+# Stack of residual blocks
+
+def stack1(x, filters, blocks, stride1=2, name=None):
+    """A set of stacked residual blocks.
+    Arguments:
+      x: input tensor.
+      filters: integer, filters of the bottleneck layer in a block.
+      blocks: integer, blocks in the stacked blocks.
+      stride1: default 2, stride of the first layer in the first block.
+      name: string, stack label.
+    Returns:
+      Output tensor for the stacked blocks.
+    """
+    x = block1(x, filters, stride=stride1, name=name + '_block1')
+    for i in range(2, blocks + 1):
+        x = block1(x, filters, conv_shortcut=False, name=name + '_block' + str(i))
+    return x
+
+
+#----------------------------------------------------------------------------
+# ResNet network
+
+def ResNet(
+        images_in,                          # Input images [batch, channel, height, width].
+        num_channels        = 3,            # Number of input color channels in images.
+        resolution          = 32,           # Resolution (h, w) of input images.
+        label_size          = 10,           # Number of labels.
+        layer_depth         = 50,           # ResNet layer depth
+        **kwargs):                          # Unused keyword args.
+
+    img_inputs = keras.Input(shape=(resolution, resolution, num_channels))
+    x = layers.Conv2D(64, 7, strides=2, padding='SAME')(images_in)
+    x = layers.BatchNormalization(axis=1)(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPooling2D(3, strides=2, padding='SAME')(x)
+    x = stack1(x, 64, 3, stride1=1, name='conv2')
+    x = stack1(x, 128, 4, name='conv3')
+    x = stack1(x, 256, 6, name='conv4')
+    x = stack1(x, 512, 3, name='conv5')
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dense(label_size, activation='softmax', name='predictions')(x)
+    model = keras.Model(inputs=img_inputs, outputs=x, name="resnet_model")
+
+    return model
+
+
+
+
+
---
+++ train.py
@@ -0,0 +1,24 @@
+from tensorflow import keras
+from networks import ResNet
+
+
+def train():
+    model = ResNet()
+    print(model.summary())
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    model.compile(
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer=keras.optimizers.RMSprop(),
+        metrics=["accuracy"],
+    )
+
+    history = model.fit(x_train, y_train, batch_size=64, epochs=2, validation_split=0.2)
+
+    test_scores = model.evaluate(x_test, y_test, verbose=2)
+    print("Test loss:", test_scores[0])
+    print("Test accuracy:", test_scores[1])
+

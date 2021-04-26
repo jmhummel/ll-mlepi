d32b4b1131ed529696fb82aaf2a37fed21c386ef
Jeremy Hummel <jeremy.hummel@rackspace.com>
2021-04-25 23:54:25 -0700

    max epochs

--- hyper_parameter_search.py
+++ hyper_parameter_search.py
@@ -4,7 +4,7 @@ from hyperopt import tpe, Trials, fmin, hp, STATUS_OK
 from train import DATASETS, train
 
 
-def search(dataset='mnist', minimize='accuracy', iterations=10):
+def search(dataset='mnist', minimize='accuracy', max_epochs=100, iterations=10):
     def objective(params):
         """Returns validation score from hyperparameters"""
         test_scores = train(int(params.get('batch_size')),
@@ -21,7 +21,7 @@ def search(dataset='mnist', minimize='accuracy', iterations=10):
     # Define the search space
     space = {
         'batch_size': hp.quniform('batch_size', 1, 256, 1),
-        'epochs': hp.quniform('epochs', 1, 100, 1),
+        'epochs': hp.quniform('epochs', 1, max_epochs, 1),
         'layer_depth': hp.quniform('layer_depth', 16, 256, 1),
         'filter_depth': hp.quniform('filter_depth', 1, 256, 1),
     }
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Hyperparameter search')
     parser.add_argument('-d', '--dataset', choices=DATASETS.keys(), help='Dataset to use')
     parser.add_argument('-m', '--minimize', choices=['accuracy', 'loss'], help='Metric to minimize')
+    parser.add_argument('-e', '--max-epochs', type=int, action='store', dest='max_epochs', help='Max epochs to train')
     parser.add_argument('-i', '--iterations', type=int, action='store', help='Iterations of search to optimize over')
     args = parser.parse_args()
     print(f'Seaching: {args}')

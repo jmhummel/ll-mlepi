ac56e746512871b6317409d54780ec2efa1f15cc
Jeremy Hummel <jeremy.hummel@rackspace.com>
2021-04-25 17:06:52 -0700

    fix load dataset

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def train(
         layer_depth = 50,       # Layer depth of ResNet
 ):
     data_source, num_channels, resolution, label_size = DATASETS[dataset]
-    (x_train, y_train), (x_test, y_test) = data_source.load_data(dataset)
+    (x_train, y_train), (x_test, y_test) = data_source.load_data()
     x_train = x_train.astype("float32") / 255
     x_test = x_test.astype("float32") / 255
 
